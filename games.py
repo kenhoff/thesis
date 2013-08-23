4ff6c2b296759bc40f6f5393c2a9a8b672cf4bfc
Ken Hoff <thekenhoff@gmail.com>
2013-08-23 15:33:50 -0600

    got images working

--- games.py
+++ games.py
@@ -6,7 +6,7 @@ class GameList:
 		gameArray = content.split('\n\n')
 		for game in gameArray:
 			info = game.split('\n')
-			self.games.add(Game(info[0], info[1]))
+			self.games.add(Game(info[0], info[1], info[2]))
 		f.close()
 
 	def latex_output(self, nestlevel = "section"):
@@ -17,12 +17,14 @@ class GameList:
 
 
 class Game:
-	def __init__(self, name, url = None, description = None):
+	def __init__(self, id, name, url = None, description = None):
 		self.name = name
 		self.url = url
 		self.description = description
+		self.id = id
 
 	def latex_output(self, nestlevel = "section"):
 		# print self.name
 		content = "\{1}{{{0.name}}}\url{{{0.url}}}".format(self, nestlevel)
+		print self.id
 		return content
