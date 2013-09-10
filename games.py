61b44812c18896ca9ba2c8a3bff09bb9ffef0434
Ken Hoff <thekenhoff@gmail.com>
2013-09-09 18:07:29 -0600

    added pdf back in - broken build

--- games.py
+++ games.py
@@ -1,23 +1,16 @@
+import json
+
 class GameList:
 	def __init__(self, infile):
 		f = open(infile, 'r')
-		content = f.read()
+		content = json.load(f)
 		self.games = set()
-		gameArray = content.split('\n\n')
+		# gameArray = content.split('\n\n')
 
+		# gameArray.pop(0) # remove keys section
 
-		###############################################
-		# useless shit i wrote to try to read in keys
-		# keys = []
-		# game_properties = gameArray[0].split('\n')
-		# for prop in game_properties:
-		# 	keys.append(prop.strip('<').strip('>'))
-
-		gameArray.pop(0) # remove keys section
-
-		for game in gameArray:
-			info = game.split('\n')
-			self.games.add(Game(info[0], info[1], info[2], info[3], info[4], info[5], info[6]))
+		for game in content["gamelist"]:
+			self.games.add(Game(game["id"], game["name"]))
 		f.close()
 
 	def latex_output(self, nestlevel = "section"):
