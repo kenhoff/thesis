4787a4c0d463163060cce69c47979460bc961836
Ken Hoff <thekenhoff@gmail.com>
2013-11-24 00:11:18 -0700

    iteration nov 23

--- games.py
+++ games.py
@@ -10,7 +10,7 @@ class GameList:
 		# gameArray.pop(0) # remove keys section
 
 		for game in content["gamelist"]:
-			self.games.append(Game(game["id"], game["title"], game["url"], game["description"], game['education'], game['images']))
+			self.games.append(Game(game["id"], game["title"], game["url"], game["description"], game['education'], game['images'], game['implementation']))
 		f.close()
 
 	def latex_output(self, nestlevel = "section"):
@@ -21,17 +21,18 @@ class GameList:
 
 
 class Game:
-	def __init__(self, id, name, url, description, education, images):
+	def __init__(self, id, name, url, description, education, images, implementation):
 		self.name = name
 		self.url = url
 		self.description = description
 		self.id = id
 		self.education = education
 		self.images = images
+		self.implementation = implementation
 
 	def latex_output(self, nestlevel = "section"):
 		# print self.name
-		content = "\\newpage" + "\{}".format(nestlevel) + "{{{}}}".format(self.name) + "\subparagraph{URL}" + "\url{{{}}}".format(self.url) + "\subparagraph{Description}" + "{}".format(self.description) + "\subparagraph{Educational Content}" + "{}".format(self.education)
+		content = "\{}".format(nestlevel) + "{{{}}}".format(self.name) + "\subparagraph{URL}" + "\url{{{}}}".format(self.url) + "\subparagraph{Description}" + "{}".format(self.description) + "\subparagraph{Educational Content}" + "{}".format(self.education) + "\subparagraph{Notes on Implementation}" + "{}".format(self.implementation) + "\\newpage"
 		for image in self.images:
 			content += "\\begin{figure}[h!]" + "\centering \includegraphics[height=0.33\\textheight]{{{0}}}".format('img/' + image['path']) + "\caption{{{0}}}".format(image['caption']) + "\end{figure}"
 		# print self.id
